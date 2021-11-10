4ae341b9b3eb51af84d14f4982e3fdd16aab853e
nkitan <ankitdas2k@gmail.com>
2021-11-10 11:11:05 +0530

    rebase

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Editor from './editor'
+import './app.css'
+
+const App: React.FC = () => {
+	return (
+	<div className='app'>
+		<Editor />
+	</div>
+	)
+}
+
+export default App
---
+++ packages/renderer/src/editor.tsx
@@ -0,0 +1,24 @@
+import React, { useCallback, useEffect } from 'react'
+import useCodeMirror from './use-codemirror'
+import './editor.css'
+
+interface Props {
+
+}
+
+const Editor: React.FC<Props> = (props) => {
+	const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+		initialDoc: 'Hello, World!',
+		onChange: () => {}
+	})
+
+	useEffect(() => {
+		if (editorView){
+				
+		}
+	}, [editorView])
+
+	return <div className='editor-wrapper' ref={refContainer}>Editor</div>
+}
+
+export default Editor
